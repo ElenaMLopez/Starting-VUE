9ea5c4c4b47bbcec414623f9774deef76b2bbb82
elenamlopez <elena.mateos1@gmail.com>
2018-05-17 14:00:59 +0200

    Added AJAX with Axios

---
+++ parte12_codigo/main12.js
@@ -0,0 +1,25 @@
+const vm1 = new Vue({
+  el: 'main',
+  // mounted es el evento que intercepta el momento en el que ya se ha montado la app
+  mounted(){
+    console.log('instancia montada');
+      this.cargarPersonas()
+  },
+  data: {
+    personas:[],
+  },
+  methods: {
+    cargarPersonas(){
+      // this.$http.get('https://randomuser.me/api/?results=50')
+      //   .then((respuesta) => {
+      //     console.log(respuesta);
+      //     this.personas = respuesta.body.results;
+      //   })
+      axios.get('https://randomuser.me/api/?results=50')
+        .then((respuesta) => {
+          console.log(respuesta);
+          this.personas = respuesta.data.results;
+        })
+    }
+  }
+});
