70c32c74e2948cedc156fd1aa4f69f945b104470
elenamlopez <elena.mateos1@gmail.com>
2018-05-21 20:31:26 +0200

    Added Component Template info

---
+++ parte14_codigo_componentes/main14.js
@@ -0,0 +1,26 @@
+Vue.component('elegir-ganador', {
+  props: ['listado'],
+  template: '',
+  methods: {
+    elegirGanador() {
+      let cantidad = this.participantes.length;
+      let indice = Math.floor(Math.random() * cantidad);
+      this.ganador = this.participantes[indice -1];
+    }
+  },
+  data() {
+    return {
+      ganador: false,
+      participantes: this.listado
+    }
+  },
+});
+
+new Vue({
+  el: 'main',
+  data: {
+    personas: [
+      'Juan', 'Alicia', 'Pedro', 'Javier', 'Marcos'
+    ]
+  },
+});
