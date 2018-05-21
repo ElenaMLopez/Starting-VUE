e8ed8ccdb49128d84b2e1ff510de0b01a12508fc
elenamlopez <elena.mateos1@gmail.com>
2018-05-21 10:26:25 +0200

    Finised Component intro

--- parte13_codigo_componentes/main13.js
+++ parte13_codigo_componentes/main13.js
@@ -1,22 +1,20 @@
 Vue.component('mis-tareas', {
-  props: ['tareas'],
   template: `<ul>
               <li v-for="tarea in tareas" v-text="tarea.title"></li>
             </ul>`,
-})
-
-const vm = new Vue({
-  el: 'main',
-  // mounted es el evento que intercepta el momento en el que ya se ha montado la app
-  mounted(){
+  mounted() {
     axios.get('https://jsonplaceholder.typicode.com/todos')
       .then((respuesta) => {
-        this.tareasAjax = respuesta.data
+        this.tareas = respuesta.data;
       })
   },
-  data: {
-    tareasAjax:[],
-  },
-  methods: {
+  data() {
+    return {
+      tareas: [],
+    }
   }
+})
+
+new Vue({
+  el: 'main',
 });
