1ca5456a04425b1b582787e0b4cce99e564cd7a3
elenamlopez <elena.mateos1@gmail.com>
2018-05-27 12:03:34 +0200

    Finished Component Comunication

--- parte20_codigo_componentes/main20.js
+++ parte20_codigo_componentes/main20.js
@@ -1,35 +1,64 @@
-Vue.component('alerta', {
-  props:['tipo', 'posicion'],
-  template: `<section class="alerta" :class="[tipo, posicion]">
-              <header class="alerta_header">
-                <a href="#" @click="ocultarWidget"> Cerrar </a>
-                <slot name="header">
-                  CUCU header (desde slot)
-                </slot>
-              </header>
-              <div class="alerta_contenido">
-                <slot>
-                   Contenido del Slot anónimo
-                </slot>
-              </div>
-              <footer class="alerta_footer">
-                <slot name="footer">
-                  Footeeerrrrr!!! Silly text (desde slot)
-                </slot>
-              </footer>
-            </section>`,
+const eventBus = new Vue();
+
+Vue.component('listado-productos', {
+  props:['productos'],
+  template: `      <section>
+          <ul>
+            <li v-for="producto in productos">
+              {{ producto.nombre }} -
+              <small> {{ producto.precio.toFixed(2) }}€</small>
+              <button @click="eliminarProducto(producto.precio)" type="button"> - </button>
+              <button @click="anadirProducto(producto.precio)" type="button"> + </button>
+            </li>
+          </ul>
+        </section>`,
   methods: {
-    ocultarWidget() {
-      this.$emit('ocultar');
+    anadirProducto(precio) {
+      eventBus.$emit('anadir', precio)
+    },
+    eliminarProducto(precio) {
+      eventBus.$emit('eliminar', precio)
     },
   }
 });
 
+Vue.component('carrito-compra', {
+    template: `
+        <section>
+            <h1> {{ total.toFixed(2) }} € </h1>
+            <h3> {{ cantidadProductos }} productos </h3>
+        </section>`,
+    data() {
+        return {
+            cantidadProductos: 0,
+            total: 0,
+        }
+    },
+    created() {
+      eventBus.$on('anadir', (precio)=> {
+        console.log('Desde el carrito',precio);
+        this.total = this.total + precio;
+        this.cantidadProductos = this.cantidadProductos +1;
+      });
+      eventBus.$on('eliminar', (precio)=> {
+        console.error('Desde el carrito', precio);
+        if (this.total >0 ) {
+          this.total = this.total - precio;
+          this.cantidadProductos = this.cantidadProductos -1;
+        }
+      })
+    },
+})
+
 new Vue({
   el: 'main',
   data: {
-    mostrarExito: false,
-    mostrarAdvertencia: false,
-    mostrarError: false,
+    productos: [
+      {nombre: 'Libro ES6', precio: 38},
+      {nombre: 'Portatil', precio: 999},
+      {nombre: 'Pen Drive', precio: 4},
+      {nombre: 'Taza Termo', precio: 10},
+      {nombre: 'Palomitas', precio: 1},
+    ],
   }
 });
